b77fd88e6c7cddd6b7676c8ad6ff664f81d7ba47
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2023-11-30 15:17:28 -0500

    Se actualiza archivo serve.js para verificar la correcta comunicación desde Postman.

--- BackendDeliveryApp/server.js
+++ BackendDeliveryApp/server.js
@@ -2,11 +2,33 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const logger = require('morgan');
+const cors = require('cors');
 
 const port = process.env.PORT || 3000;
 app.set('port', port);
 
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({
+    extended : true,
+}));
+
+app.use(cors());
+
+app.disable('x.powered-by');
+
 // Verificar la ip con ipconfig
 server.listen(3000, '192.168.1.3' || 'localhost', function(){
     console.log( 'Aplicación de NodeJS en el puerto ' + port );
+});
+
+app.get('/', (req, res) => {
+    res.send('Ruta raíz del backend');
+});
+
+// Error Handler
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(err.status || 500).send(err.stack);
 });
