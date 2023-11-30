967aad98ab0db872dd38e5a5c54ea806b0f7ced8
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2023-11-29 22:39:45 -0500

    Se agrega una imágen en el proyecto que se mostrará cada vez que se inicie la aplicación.

--- flutter_delivery/lib/src/pages/login/login_page.dart
+++ flutter_delivery/lib/src/pages/login/login_page.dart
@@ -5,11 +5,23 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Scaffold es la maquetación de nuestra aplicación.
-    return const Scaffold(
-      body: Center(
-        child: Text('LOGIN PAGE'),
+    /// Scaffold es la maquetación de nuestra aplicación.
+    return Scaffold(
+      /// El Stack nos permite dar un posicionamiento a diferentes elementos o contenedores.
+      body: Stack(
+        /// Llamo el método que carga la imágen principal de la aplicación.
+        children: [
+          _imageCover()
+        ],
       ),
+    );
+  }
+
+  /// Cada vez que coloco un _ antes del nombre del método indica que ese método sera PRIVATE.
+  Widget _imageCover(){
+    return Image.asset('assets/img/delivery.png',
+    width: 130,
+    height: 130,
     );
   }
 }
