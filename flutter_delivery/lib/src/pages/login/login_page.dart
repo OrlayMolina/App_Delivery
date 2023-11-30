89c86501c59cb15c79d2c286a040baad5bf4474e
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2023-11-29 22:10:02 -0500

    Se corrige error al momento de realizar el debug de un archivo Kotlin

---
+++ flutter_delivery/lib/src/pages/login/login_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Scaffold es la maquetación de nuestra aplicación.
+    return const Scaffold(
+      body: Center(
+        child: Text('LOGIN PAGE'),
+      ),
+    );
+  }
+}
