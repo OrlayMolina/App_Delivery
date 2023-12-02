5db678ed75fc5fc3d4321b3f9b9cd249c807e76a
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2023-12-02 18:58:15 -0500

    Se actualiza el método _imageUser.

--- flutter_delivery/lib/src/environment/environment.dart
+++ flutter_delivery/lib/src/environment/environment.dart
@@ -1,5 +1,5 @@
 class Environment {
 
   /// Variable con la ruta para hacer las peticiones a NodeJs.
-  static const String API_URL = "http://192.168.1.1:3000/";
+  static const String API_URL = "http://192.168.1.3:3000/";
 }
