d69880a80c1e1022b2921924309ec7a1ccdbda47
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2023-12-22 16:15:39 -0500

    Se crean nuevos controladores de la aplicación.

---
+++ flutter_delivery/lib/src/pages/client/products/list/client_products_list_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_delivery/src/pages/client/products/list/client_products_list_controller.dart';
+import 'package:get/get.dart';
+
+class ClientProductsListPage extends StatelessWidget {
+
+  ClientProductsListController controller = Get.put(ClientProductsListController());
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
